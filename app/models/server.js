98a78bfbf53abadfbf4841ebe4a0474b80304f02
racavi <63754467+racavi@users.noreply.github.com>
2023-07-05 17:14:38 +0200

    Provide CORS settings

--- app/models/server.js
+++ app/models/server.js
@@ -1,5 +1,6 @@
 
 const express = require('express');
+const cors = require('cors');
 
 class Server {
   #app;
@@ -8,7 +9,12 @@ class Server {
   constructor() {
     this.#app = express();
     this.#port = process.env.PORT;
+    this.#middlewares();
     this.#routes();
+  }
+
+  #middlewares() {
+    this.#app.use(cors());
   }
 
   #routes() {
