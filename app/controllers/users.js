bc43418c415d623f0b99ebbf9b683e3c68fe0d07
racavi <63754467+racavi@users.noreply.github.com>
2023-07-07 13:39:56 +0200

    Add controller to handle User creation via POST

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,11 +1,25 @@
 const { response } = require('express');
 
+const User = require('../models/user');
+
 const usersGet = ( req, res = response) => {
     res.json({
         message: 'Hello World!'
     });
 }
 
+const usersPost = async( req, res = response) => {
+    const { mail } = req.body;
+
+    const user = new User({ mail });
+    user.save();
+
+    res.json({
+        user
+    });
+}
+
 module.exports = {
     usersGet,
+    usersPost
 }
