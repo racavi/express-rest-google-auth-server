4ba11644299846af973d595b1a726e0ad10bc78e
racavi <63754467+racavi@users.noreply.github.com>
2023-07-07 09:08:56 +0200

    Provide DB connectivity function

--- app/models/server.js
+++ app/models/server.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const cors = require('cors');
 
+const { dbConnection } = require('../db/config');
+
 class Server {
   #app;
   #port;
@@ -10,10 +12,15 @@ class Server {
   constructor() {
     this.#app = express();
     this.#port = process.env.PORT;
+    this.#connectDb();
     this.#middlewares();
     this.#routes();
   }
 
+  async #connectDb() {
+    await dbConnection();
+  }
+  
   #middlewares() {
     this.#app.use(cors());
   }
