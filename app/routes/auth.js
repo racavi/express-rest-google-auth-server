4f881f14ebf98d4d821c643e263b310a55d5cb69
racavi <63754467+racavi@users.noreply.github.com>
2023-08-23 13:33:13 +0200

    Add controller to authenticate User via access token

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -2,14 +2,19 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 
 const { validateFields } = require('../middlewares/validate-fields');
+const { verifyAccessJWT } = require('../middlewares/verify-access-jwt')
 
-
-const { googleSignIn } = require('../controllers/auth');
+const { googleSignIn, authenticate } = require('../controllers/auth');
 
 const router = Router();
+
 router.post('/google/sign-in', [
     check('id_token', 'id_token is mandatory.').not().isEmpty(),
     validateFields
 ], googleSignIn);
+
+router.get('/authenticate', [
+    verifyAccessJWT,
+], authenticate)
 
 module.exports = router;
