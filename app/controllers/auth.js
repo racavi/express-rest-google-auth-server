b7428e2afd1cd53f5b5735745263f74218ae36f3
racavi <63754467+racavi@users.noreply.github.com>
2023-08-25 14:12:25 +0200

    Auth | Refresh Token | Add controller for refreshing Access JWT

--- app/controllers/auth.js
+++ app/controllers/auth.js
@@ -6,7 +6,7 @@ const { verifyGoogleIdToken } = require('../helpers/google-validators');
 const { generateJWT, generateRefreshJWT } = require('../helpers/jwt-generator');
 
 
-const googleSignIn = async(req=request, res=response) => {
+const googleSignIn = async (req = request, res = response) => {
 
     const { id_token } = req.body;
 
@@ -15,7 +15,7 @@ const googleSignIn = async(req=request, res=response) => {
 
         let user = await User.findOne({ mail });
 
-        if ( !user ) {
+        if (!user) {
             const user_data = {
                 mail,
             }
@@ -42,11 +42,11 @@ const googleSignIn = async(req=request, res=response) => {
 // @desc Authenticates User via access token
 // @route GET /api/v1/auth/authenticate
 // @access Private - Valid Access JWT is required to use it
-const authenticate = async(req=request, res=response) => {
+const authenticate = async (req = request, res = response) => {
     const id = req.uid;
     const { mail } = await User.findById(id).exec();
 
-    if ( mail ) {
+    if (mail) {
         return res.status(200).json({
             mail
         });
@@ -57,7 +57,37 @@ const authenticate = async(req=request, res=response) => {
     })
 }
 
+// @desc Generates a new Access Token from a valid Refresh Token
+// @route GET /api/v1/auth/refresh
+// @access Public - Everyone is allowed to send a Refresh Tokens
+const refresh_token = async (req = request, res = response) => {
+    const { uid, refresh_token } = req;
+    const userFound = await User.findById(uid).exec();
+    if (!userFound) return res.status(401).json({ message: 'Bad credentials' })
+    // TODO: pick User Refresh token from DB
+
+    // TODO: compare refresh token sent within the request body with former one
+
+    // TODO: if they match, generate a new Access JWT 201
+    const refresh_tokens_match = true
+
+    if (refresh_tokens_match) {
+        const access_token = await generateJWT(uid);
+        // console.log('new access token:', access_token)
+        return res.status(201).json({
+            access_token
+        });
+    }
+
+    // TODO: If they don't match send error maessage - 401
+    return res.status(401).json({
+        message: 'Bad credentials'
+    })
+
+}
+
 module.exports = {
     googleSignIn,
     authenticate,
+    refresh_token,
 }
